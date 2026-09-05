6d7e2407da620b6e9ea862ac27df527159f33a5f
Chip Johansen <chip.johansen99@gmail.com>
2026-09-05 06:39:47 -0400

    setting things up

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 class Person:
-    # write your code here
-    pass
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
 
 
 def create_person_list(people: list) -> list:
