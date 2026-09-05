dcb31c974e6cc025fa6679d20c0dbdd9cb63d877
Chip Johansen <chip.johansen99@gmail.com>
2026-09-05 07:10:16 -0400

    fix flake8 crap

--- app/main.py
+++ app/main.py
@@ -7,21 +7,20 @@ class Person:
         Person.people[name] = self
 
 
-
 def create_person_list(people: dict) -> list:
     result = []
     for person_dict in people:
-        name = person_dict['name']
-        age = person_dict['age']
+        name = person_dict["name"]
+        age = person_dict["age"]
         # Create the Person instance (it will be added to Person.people)
         person = Person(name, age)
-        
+
         # Check if there's a wife or husband key
-        if 'wife' in person_dict and person_dict['wife'] is not None:
-            person.wife = Person.people[person_dict['wife']]
-        elif 'husband' in person_dict and person_dict['husband'] is not None:
-            person.husband = Person.people[person_dict['husband']]
-        
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            person.wife = Person.people[person_dict["wife"]]
+        elif "husband" in person_dict and person_dict["husband"] is not None:
+            person.husband = Person.people[person_dict["husband"]]
+
         result.append(person)
-    
+
     return result
